9f10c306c5f56be86d65a57abda590dc14d313ef
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2022-12-22 12:12:37 +0600

    Delete ternary operator directory

--- ternary operator/ternaryPosNeg.js
+++
@@ -1,8 +0,0 @@
-/*var num = Number(prompt("enter a number: "));
-var result = num > 0 ? "positive" : "negative";
-document.write(result);
-*/
-
-var num = Number(prompt("enter a number: "));
-var result = num > 0 ? "positive" : num < 0 ? "negative" : "Zero";
-document.write(result);
