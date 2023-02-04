00c0861824d284d6cc5500342e5e4721a382c4dc
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-02-04 19:20:33 +0600

    CSS Style manipulating

---
+++ jQuery/practice3.js
@@ -0,0 +1,36 @@
+$("h1").addClass("style style2");
+
+/* css code
+.style {
+  font-size: 36px;
+  color: white;
+  background-color: red;
+  padding: 10px;
+  margin: 10px 20px;
+}
+
+.style2 {
+  font-family: italic;
+  cursor: pointer;
+  text-align: center;
+  font-weight: bold;
+}
+ */
+
+/* html code
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>This is heading</h1>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="index.js"></script>
+  </body>
+</html>
+*/
