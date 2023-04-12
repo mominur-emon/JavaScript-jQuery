0e86e2107a044811aea0c5ab1835263c6682b8f0
Mominur Rahman Emon <109852788+mominur-emon@users.noreply.github.com>
2023-04-12 15:11:28 +0600

    calling api from javascript - ajax jquery

---
+++ API/ajax jquery/jquery.js
@@ -0,0 +1,22 @@
+console.clear();
+//console.log(window);
+
+const makeRequest = async (url, method) => {
+  try {
+    const result = await $.ajax({
+      url: url,
+      method: method,
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts/1", "GET").then(
+    (res) => console.log(res)
+  );
+};
+
+getData();
